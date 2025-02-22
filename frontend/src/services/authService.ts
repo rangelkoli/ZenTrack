c8ad24c11fe7f89ba181a578927058ae8460370a
Rangel Koli <77476841+rangelkoli@users.noreply.github.com>
2025-02-21 22:17:41 -0500

    Update API URL in authService to include '/auth' endpoint

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const API_URL = import.meta.env.VITE_BACKEND_URL;
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/auth`
 
 interface LoginResponse {
   access_token: string;
