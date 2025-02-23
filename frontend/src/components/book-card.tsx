5da223dcbb7cc12dd432bb1f83ec63b9256cf01a
Rangel Koli <77476841+rangelkoli@users.noreply.github.com>
2025-02-22 22:38:40 -0500

    Refactor BookCard component to use Link for navigation to notes

--- frontend/src/components/book-card.tsx
+++ frontend/src/components/book-card.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Link } from "react-router";
 
 interface BookCardProps {
   title: string;
@@ -10,33 +11,32 @@ interface BookCardProps {
 
 export function BookCard({ title, cover_image, id }: BookCardProps) {
   return (
-    <Card
-      className='group relative overflow-hidden border-0 bg-transparent shrink-0'
-      onClick={() => (window.location.href = `/notes/${id}`)}
-    >
-      <CardContent className='p-0'>
-        <div className='relative h-64 w-80'>
-          {cover_image ? (
-            <img
-              src={cover_image}
-              alt={title}
-              className='rounded-lg object-cover h-full w-full'
-              sizes=''
-            />
-          ) : (
-            <img
-              src='https://placehold.co/800x750'
-              alt='Placeholder'
-              className='rounded-lg object-fill'
-              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-            />
-          )}
-          <div className='absolute inset-0 bg-gradient-to-t from-black/60 to-transparent rounded-lg' />
-          <div className='absolute bottom-0 left-0 right-0 p-4 text-white'>
-            <h3 className='text-md font-semibold line-clamp-2'>{title}</h3>
+    <Card className='group relative overflow-hidden border-0 bg-transparent shrink-0'>
+      <Link to={`/notes/${id}`} className='block'>
+        <CardContent className='p-0'>
+          <div className='relative h-64 w-80'>
+            {cover_image ? (
+              <img
+                src={cover_image}
+                alt={title}
+                className='rounded-lg object-cover h-full w-full'
+                sizes=''
+              />
+            ) : (
+              <img
+                src='https://placehold.co/800x750'
+                alt='Placeholder'
+                className='rounded-lg object-fill'
+                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+              />
+            )}
+            <div className='absolute inset-0 bg-gradient-to-t from-black/60 to-transparent rounded-lg' />
+            <div className='absolute bottom-0 left-0 right-0 p-4 text-white'>
+              <h3 className='text-md font-semibold line-clamp-2'>{title}</h3>
+            </div>
           </div>
-        </div>
-      </CardContent>
+        </CardContent>
+      </Link>
     </Card>
   );
 }
